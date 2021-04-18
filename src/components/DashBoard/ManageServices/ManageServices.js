0edde646010c8ed63bb177e4b087125e0f1d74f1
Debashis Ray <debaray14@gmail.com>
2021-04-18 17:16:43 +0600

    manage services done

--- src/components/DashBoard/ManageServices/ManageServices.js
+++ src/components/DashBoard/ManageServices/ManageServices.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'react-bootstrap';
 import NavBar from '../../Shared/NavBar/NavBar';
 import SideBar from '../SideBar/SideBar';
+import Spinner from '../../Shared/Spinner/Spinner';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const ManageServices = () => {
+    const [servicesData, setServiceData] = useState([]);
+    const [showSpin, setShowSpin] = useState(false);
+    useEffect(() => {
+        const url = `http://localhost:5000/services`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setServiceData(data);
+                setShowSpin(true);
+            })
+    }, [showSpin])
+    const deleteService = id => {
+        fetch(`http://localhost:5000/deleteService/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => console.log(res))
+            .then(data => {
+                console.log('delete done');
+                alert('Delete This Service Successfully');
+            })
+            .catch(err => { console.log(err) })
+        setShowSpin(false)
+    }
     return (
         <div>
             <NavBar></NavBar>
@@ -11,7 +38,31 @@ const ManageServices = () => {
                     <SideBar></SideBar>
                 </div>
                 <div className="col-lg-9">
-                    <h1>This is ManageServices</h1>
+                    <h3 className="mt-2">Manage Services</h3>
+                    {!showSpin && <Spinner></Spinner>}
+                    {showSpin && <Table className="mt-3" bordered responsive hover size="sm">
+                        <thead>
+                            <tr>
+                                <th className="text-center">#</th>
+                                <th className="text-center">Service Name</th>
+                                <th className="text-center">Service Description</th>
+                                <th className="text-center">Image</th>
+                                <th className="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {servicesData.map((service,index) =>
+
+                                <tr>
+                                     <td style={{width:'3%'}}>{index+1}</td>
+                                    <td style={{width:'20%'}}>{service.title}</td>
+                                    <td style={{width:'45%'}}>{service.description}</td>
+                                    <td style={{width:'20%'}}>
+                                        <img style={{height:'150px'}} className="img-fluid border" src={`data:image/png;base64,${service.image.img}`} alt="" /></td>
+                                        <td style={{width:'12%'}} className="text-end"><Button variant="danger" onClick={() => deleteService(`${service._id}`)}><FontAwesomeIcon icon={faTrash} /> Delete</Button></td>
+                                </tr>)}
+                        </tbody>
+                    </Table>}
                 </div>
             </div>
         </div>
